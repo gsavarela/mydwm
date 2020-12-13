bbd4530bf4960a21a24fb5c4d4ebb24384eefec4
Varela <guilhermevarela@protonmail.com>
2020-12-13 00:38:08 +0000

    Change color variable names && Add comments

--- config.h
+++ config.h
@@ -6,17 +6,21 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+/* FONTS */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+
+/* COLORSCHEMES */
+static const char normfgcolor[]       = "#bbbbbb";
+static const char normbgcolor[]       = "#222222";
+static const char normbdcolor[]       = "#444444";
+static const char selfgcolor[]        = "#eeeeee";
+static const char selbgcolor[]        = "#005577";
+static const char selbdcolor[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { normfgcolor, normbgcolor, normbdcolor },
+	[SchemeSel]  = { selfgcolor, selbgcolor,  selbdcolor  },
 };
 
 /* tagging */
@@ -57,7 +61,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbdcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 #include "shiftview.c"
