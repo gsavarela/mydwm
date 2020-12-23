a7eabdb8c4fd199c38b02a82f303c46ff820e2bc
Varela <guilhermevarela@protonmail.com>
2020-12-23 20:58:32 +0000

    Add Volume increase and volume decrease bindings
        * ModKey, XK_z  increase volume by 10%
        * ModKey|ShiftMask, XK_z  decrease volume by 10%

--- config.def.h
+++ config.def.h
@@ -95,6 +95,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+	{ MODKEY,                       XK_z,      spawn,          SHCMD("pulsemixer --change-volume +10") },
+	{ MODKEY|ShiftMask,             XK_z,      spawn,          SHCMD("pulsemixer --change-volume -10") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
